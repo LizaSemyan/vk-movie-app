e3bf2c95c93eb527c022dea84459e0c4ac5fffc5
LizaSemyan <liza777sem@icloud.com>
2026-03-22 20:02:14 +0300

    fix: show year if no premiere

--- src/utils/getMovieMeta.ts
+++ src/utils/getMovieMeta.ts
@@ -2,18 +2,23 @@ import type { MovieDetails } from "../types/movie";
 import { formatDate } from "./formatDate";
 
 export const getMovieMeta = (movie: MovieDetails | undefined) => {
+  const premiereDate =
+    movie?.premiere?.world ||
+    movie?.premiere?.russia ||
+    movie?.premiere?.digital ||
+    movie?.premiere?.cinema;
+
   return {
     title: movie?.name || movie?.alternativeName || "Без названия",
     posterUrl: movie?.poster?.url || movie?.poster?.previewUrl,
     description:
       movie?.description || movie?.shortDescription || "Описание отсутствует",
     rating: movie?.rating?.kp || movie?.rating?.imdb || "—",
-    releaseDate: formatDate(
-      movie?.premiere?.world ||
-        movie?.premiere?.russia ||
-        movie?.premiere?.digital ||
-        movie?.premiere?.cinema,
-    ),
+    releaseDate: premiereDate
+      ? formatDate(premiereDate)
+      : movie?.year
+        ? String(movie.year)
+        : "—",
 
     genres: movie?.genres ?? [],
   };
